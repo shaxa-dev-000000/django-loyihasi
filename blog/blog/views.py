4c0b670c6a19a77602b9e72124c418cfdc8ba11f
shaxa-dev-000000 <shaxriyorismatov14@gmail.com>
2025-01-02 08:32:49 +0500

    django amaliyot uchun

---
+++ blog/blog/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render # type: ignore
+from django.urls import reverse_lazy # type: ignore
+from django.views.generic import ListView, DetailView # type: ignore
+from django.views.generic.edit import CreateView, UpdateView, DeleteView # type: ignore
+from .models import Post
+
+# Create your views here.
+class BlogListView(ListView):
+    model = Post
+    template_name = 'home.html'
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'detail_view.html'
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = ['title', 'author', 'body']
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
